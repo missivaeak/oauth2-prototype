d3f15234ba3b35cedf1fe5e7480bfefcd089251c
Adam Esbjörnsson <ades22@student.bth.se>
2023-11-19 12:00:20 +0100

    corrections

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,54 @@
 var express = require('express');
 var router = express.Router();
-const { OAuthApp, Octokit } = require('octokit')
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  const response = await fetch("http://localhost:1337/customer/authurl");
-  const result = await response.json();
 
-  res.render('index', { authUrl: result.data.url });
-});
 
 router.get('/authcallback', async function(req, res, next) {
   const code = req.query.code;
   const state = req.query.state;
-  const response = await fetch(`http://localhost:1337/customer/auth?code=${code}&state=${state}`);
+
+  const response = await fetch(`http://localhost:1337/customer/auth`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ code, state }),
+    redirect: 'follow'
+  });
   const result = await response.json();
 
   res.render('authcallback', { oAuthToken: result.authentication?.token ?? "failed" });
 });
 
 router.get('/jwt', async function(req, res, next) {
-  const token = req.query.token;
-  const response = await fetch(`http://localhost:1337/customer/token?token=${token}`);
+  const oAuthToken = req.query.token;
+  const response = await fetch(`http://localhost:1337/customer/token`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      oAuthToken
+    }),
+    redirect: 'follow'
+  });
   let result;
 
   try {
     result = await response.json()
-    console.log(result)
   } catch (error) {
     console.error(error);
   }
 
   res.render('jwt', { data: result?.data ?? "failed" });
+});
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const response = await fetch("http://localhost:1337/customer/auth");
+  const result = await response.json();
+
+  res.render('index', { authUrl: result.data.url });
 });
 
 module.exports = router;
